c557b38576e7907ccab2fca99df6c6c74c6a483e
律澗隱 <adler.lei@gmail.com>
2024-11-01 04:02:11 +0800

    開發中

---
+++ js/navbar_burger.js
@@ -0,0 +1,24 @@
+// navbar burger
+document.addEventListener('DOMContentLoaded', () => {
+
+    const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+
+    $navbarBurgers.forEach(el => {
+        el.addEventListener('click', () => {
+            const target = el.dataset.target;
+            const $target = document.getElementById(target);
+            el.classList.toggle('is-active');
+            $target.classList.toggle('is-active');
+
+        });
+    });
+});
+
+// navbar burger hidden |
+function navbar_burger_hidden() {
+    const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-item is-hidden-touch'), 0);
+    $navbarBurgers.forEach(el => {
+        el.classList.toggle('is-hidden-touch');
+    });
+
+}
