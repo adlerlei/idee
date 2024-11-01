d6ad54ea27583c3b4938fcd0f9d41b617f539bf7
律澗隱 <lujianyin@lujianyindeiMac.local>
2024-11-01 19:01:15 +0800

    navbar top and footer buttom

--- js/navbar_burger.js
+++ js/navbar_burger.js
@@ -1,24 +1,21 @@
-// navbar burger
-document.addEventListener('DOMContentLoaded', () => {
+// navbar burger trigger
+document.addEventListener("DOMContentLoaded", () => {
+    const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll(".navbar-burger"), 0);
 
-    const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
-
-    $navbarBurgers.forEach(el => {
-        el.addEventListener('click', () => {
+    $navbarBurgers.forEach((el) => {
+        el.addEventListener("click", () => {
             const target = el.dataset.target;
             const $target = document.getElementById(target);
-            el.classList.toggle('is-active');
-            $target.classList.toggle('is-active');
-
+            el.classList.toggle("is-active");
+            $target.classList.toggle("is-active");
         });
     });
 });
 
 // navbar burger hidden |
 function navbar_burger_hidden() {
-    const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-item is-hidden-touch'), 0);
-    $navbarBurgers.forEach(el => {
-        el.classList.toggle('is-hidden-touch');
+    const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll(".navbar-item.is-hidden-touch"), 0);
+    $navbarBurgers.forEach((el) => {
+        el.classList.toggle("is-hidden-touch");
     });
-
 }
