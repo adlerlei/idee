d29700d092aa13bde3f808f83cec80156837343b
律澗隱 <adler.lei@gmail.com>
2024-11-02 20:20:11 +0800

    Uikit navbar 完成

--- js/navbar_burger.js
+++ js/navbar_burger.js
@@ -1,21 +1,24 @@
-// navbar burger trigger
-document.addEventListener("DOMContentLoaded", () => {
-    const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll(".navbar-burger"), 0);
+// navbar burger
+document.addEventListener('DOMContentLoaded', () => {
 
-    $navbarBurgers.forEach((el) => {
-        el.addEventListener("click", () => {
+    const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+
+    $navbarBurgers.forEach(el => {
+        el.addEventListener('click', () => {
             const target = el.dataset.target;
             const $target = document.getElementById(target);
-            el.classList.toggle("is-active");
-            $target.classList.toggle("is-active");
+            el.classList.toggle('is-active');
+            $target.classList.toggle('is-active');
+
         });
     });
 });
 
 // navbar burger hidden |
 function navbar_burger_hidden() {
-    const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll(".navbar-item.is-hidden-touch"), 0);
-    $navbarBurgers.forEach((el) => {
-        el.classList.toggle("is-hidden-touch");
+    const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-item is-hidden-touch'), 0);
+    $navbarBurgers.forEach(el => {
+        el.classList.toggle('is-hidden-touch');
     });
+
 }
