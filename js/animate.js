40f5bc4577438c114d73fb75ce22b8d0a5aab834
Adler Lei <adler.lei@gmail.com>
2024-12-23 17:11:51 +0800

    contact.html head

--- js/animate.js
+++ js/animate.js
@@ -29,7 +29,25 @@ const observer = new IntersectionObserver((entries) => {
     });
   });
   
-  // 選擇所有需要動畫的元素
+  // 選擇所有需要動畫的元素  animate__animated animate__fadeInLeft
   document.querySelectorAll('.project-animate').forEach((element) => {
     projectObserver.observe(element);
   });
+
+  // contact.html
+const contactObserver = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    // 當元素進入視窗
+    if (entry.isIntersecting) {
+      // 添加動畫 class
+      entry.target.classList.add('animate__animated', 'animate__fadeInLeft');
+      // 如果你不想要重複動畫，可以取消觀察
+      // contactObserver.unobserve(entry.target);
+    }
+  });
+});
+
+// 選擇所有需要動畫的元素
+document.querySelectorAll('.contact-animate').forEach((element) => {
+  contactObserver.observe(element);
+});
